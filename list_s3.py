faf375c0ce9ad4c32eedbc6babbabea3a80ab120
Alejandro <achdevops@gmail.com>
2020-04-14 15:10:45 -0300

    update my s3 lab

--- list_s3.py
+++ list_s3.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
+# create with good vibes by: @chaconmelgarejo
+# description: py-labs for test purpose - list s3 buckets
+
 import boto3
 
-boto3.setup_default_session(profile_name='cloudsultor')
+profile = input("Insert your aws profile: ")
+
+boto3.setup_default_session(profile_name=profile)
 # Retrieve the list of existing buckets
 s3 = boto3.client('s3')
 response = s3.list_buckets()
